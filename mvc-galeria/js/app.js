418d40ea62d63e55e2282ff594035acc277c42b5
Wesnei <wesneipaiva@gmail.com>
2025-08-21 14:44:25 -0300

    fix: correction folder

--- mvc-galeria/js/app.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const model = new ImageModel();
-    const view = new ImageView();
-    const controller = new ImageController(model, view);
-});
